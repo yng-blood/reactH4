09d760affc4c2eaefe3d52e3470be4ac528123bf
yng-blood <mfstarboey@gmail.com>
2023-01-31 14:30:58 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import {BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-
+import Contactus from'./Component/Contactus.jsx'
+import Student from './Component/Student';
+import Header from './Component/Header';
+import Home from './Component/Home';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header/>
+    <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/contactus' element={<Contactus/>}/>
+          <Route path='/Student' element={<Student/>}/>
+    </Routes>
+   
+    </>
   );
 }
+
 
 export default App;
